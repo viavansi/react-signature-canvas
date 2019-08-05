ce322d150587c7f1b6355f6bbc7674126c49d0b1
Juan G. Hurtado <juan.g.hurtado@gmail.com>
2019-08-05 12:55:17 +0200

    Change dependency to signature_pad_viafirma

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
-import SignaturePad from 'signature_pad'
+import SignaturePad from 'signature_pad_viafirma'
 import trimCanvas from 'trim-canvas'
 
 export default class SignatureCanvas extends Component {
